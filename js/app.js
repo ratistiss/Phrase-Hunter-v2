524e364bb31a6be8f8f69c94481256798887c71a
ratistiss <50683985+ratistiss@users.noreply.github.com>
2019-09-16 17:22:40 -0400

    Coding 1.5
    Start Button connected to startGame();

--- js/app.js
+++ js/app.js
@@ -2,6 +2,12 @@
  * Project 4 - OOP Game App
   app.js */
 
-const game = new Game();
-game.startGame();
-console.log(`Active Phrase - phrase: ${game.activePhrase.phrase}`);
+const game = {};
+
+
+document.querySelector('button').addEventListener("click", () => {
+    this.game = new Game();
+    this.game.startGame();
+});
+
+
